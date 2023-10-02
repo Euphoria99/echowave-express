fbf188acd8d97b1eb7785962e7c25954c5fb3e9f
Euphoria99 <363pavanbhat@gmail.com>
2023-10-02 23:14:26 +0530

    Fetching message history from DB

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const cookieParser = require('cookie-parser')
-const bcrypt = require('bcryptjs')
-const ws = require('ws')
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const ws = require("ws");
 
 const User = require("./models/User");
 const Message = require("./models/Message");
@@ -23,96 +23,121 @@ app.use(
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(500).send("Something went wrong!");
 });
 
-
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log('mongoose connection successful'))
-.catch( (err) => console.log('mongoose error', err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("mongoose connection successful"))
+  .catch((err) => console.log("mongoose error", err));
 const jwtSecret = process.env.JWT_SECRET;
-const bcryptSalt = bcrypt.genSaltSync(10)
-
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.get("/", (req, res) => {
   res.json("Hello world");
 });
 
+async function getUserDataFromRequest(req) {
+  return new Promise( (resolve, reject) => {
+    const token = req.cookies?.token;
+    if (token) {
+      jwt.verify(token, jwtSecret, {}, (err, userData) => {
+        if (err) throw err;
+        resolve(userData);
+      });
+    } else {
+      reject('No Token');
+    }
+  });
+}
+
 app.post("/register", async (req, res) => {
-    console.log('called')
+  console.log("called");
   const { username, password } = req.body;
   try {
-
-    const hashedPassword = bcrypt.hashSync(password, bcryptSalt)
-    const createdUser = await User.create({ 
-      username:username, 
-      password:hashedPassword
-     });
-    jwt.sign({ userId: createdUser._id, username }, jwtSecret, {}, (err, token) => {
-      console.log("the token", token);
-      if (err) throw err;
-      res.cookie('token', token, {sameSite:'none', secure: true}).status(201).json({
-        id: createdUser._id,
-      });
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+    const createdUser = await User.create({
+      username: username,
+      password: hashedPassword,
     });
+    jwt.sign(
+      { userId: createdUser._id, username },
+      jwtSecret,
+      {},
+      (err, token) => {
+        console.log("the token", token);
+        if (err) throw err;
+        res
+          .cookie("token", token, { sameSite: "none", secure: true })
+          .status(201)
+          .json({
+            id: createdUser._id,
+          });
+      }
+    );
   } catch (err) {
     if (err) throw err;
-    res.status(500).json('error')
+    res.status(500).json("error");
   }
 });
 
-app.post('/login', async (req, res) => {
-  const { username, password} = req.body;
-  const foundUser = await User.findOne({ username});
-  if(foundUser){
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const foundUser = await User.findOne({ username });
+  if (foundUser) {
     const passOk = bcrypt.compareSync(password, foundUser.password);
-    if(passOk){
-      jwt.sign({ userId: foundUser._id, username }, jwtSecret, {}, (err, token) => {
-        if (err) throw err;
-        res.cookie('token', token, {sameSite:'none', secure: true}).json({
-          id: foundUser._id,
-        });
-      });
+    if (passOk) {
+      jwt.sign(
+        { userId: foundUser._id, username },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token, { sameSite: "none", secure: true }).json({
+            id: foundUser._id,
+          });
+        }
+      );
     }
   }
-})
+});
 
-app.get('/profile', (req, res) => {
+app.get("/profile", (req, res) => {
   const token = req.cookies?.token;
-  if(token){
+  if (token) {
     jwt.verify(token, jwtSecret, {}, (err, userData) => {
-      if(err) throw err;
+      if (err) throw err;
       res.json(userData);
-    })
-  }else {
-    res.status(401).json('no token')
+    });
+  } else {
+    res.status(401).json("no token");
   }
-})
+});
 
 const server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
-
-const wss = new ws.WebSocketServer({server});
-wss.on('connection', (connection,req) => {
-
+const wss = new ws.WebSocketServer({ server });
+wss.on("connection", (connection, req) => {
   //read username and id from the cookie for this connection
   const cookies = req.headers.cookie;
-  if(cookies){
-    const tokenCookieString = cookies.split(';').find( str => str.startsWith('token='));
-    console.log('The split token', tokenCookieString);
-    if(tokenCookieString){
-      const token = tokenCookieString.split('=')[1];
-      console.log("🚀 ~ file: index.js:105 ~ wss.on ~ token:", token)
-      if(token){
+  if (cookies) {
+    const tokenCookieString = cookies
+      .split(";")
+      .find((str) => str.startsWith("token="));
+    console.log("The split token", tokenCookieString);
+    if (tokenCookieString) {
+      const token = tokenCookieString.split("=")[1];
+      console.log("🚀 ~ file: index.js:105 ~ wss.on ~ token:", token);
+      if (token) {
         jwt.verify(token, jwtSecret, {}, (err, userData) => {
           if (err) throw err;
-          const {userId, username} = userData;
+          const { userId, username } = userData;
           connection.userId = userId;
           connection.username = username;
-        })
+        });
       }
     }
   }
@@ -120,37 +145,57 @@ wss.on('connection', (connection,req) => {
   //grab all the clients from websocket server
   //notify everyone about online people(when someone connects)
 
-  console.log('no of clients', [...wss.clients].map(c => c.username));
+  console.log(
+    "no of clients",
+    [...wss.clients].map((c) => c.username)
+  );
 
-  [...wss.clients].forEach(client => {
-    client.send(JSON.stringify({
-      online: [...wss.clients].map(c => ({userId:c.userId, username:c.username}))  
-    }));
+  [...wss.clients].forEach((client) => {
+    client.send(
+      JSON.stringify({
+        online: [...wss.clients].map((c) => ({
+          userId: c.userId,
+          username: c.username,
+        })),
+      })
+    );
   });
-    
 
-  connection.on('message', async (message) => {
+  connection.on("message", async (message) => {
     const messageData = JSON.parse(message.toString());
-    console.log('The message-->', messageData );
-    const { recipient, text} = messageData;
-    if( recipient && text){
-
+    console.log("The message-->", messageData);
+    const { recipient, text } = messageData;
+    if (recipient && text) {
       const messageDoc = await Message.create({
         sender: connection.userId,
         recipient,
         text,
       });
       [...wss.clients]
-      .filter( c => c.userId === recipient)
-      .forEach( c => c.send(JSON.stringify({
-        //sending message
-        text, 
-        sender:connection.userId, 
-        recipient,
-        id: messageDoc._id
-      })))
+        .filter((c) => c.userId === recipient)
+        .forEach((c) =>
+          c.send(
+            JSON.stringify({
+              //sending message
+              text,
+              sender: connection.userId,
+              recipient,
+              id: messageDoc._id,
+            })
+          )
+        );
     }
-  }
-  
-  )
+  });
+});
+
+app.get('/messages/:userId', async (req, res) => {
+  const {userId} = req.params;
+  const userData = await getUserDataFromRequest(req);
+  const ourUserId = userData.userId;
+  console.log('The user ids', {userId, ourUserId})
+  const messages =  await Message.find({
+    sender:{$in : [userId, ourUserId]} ,
+    recipient: {$in : [userId, ourUserId]}
+  }).sort({createdAt : -1});
+  res.json(messages)
 })
