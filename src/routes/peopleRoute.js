3c8b9c61a77b5ad916a2c3eb3291d7e9aeb5fead
Pavan Bhat <57312042+Euphoria99@users.noreply.github.com>
2023-11-05 18:22:42 +0530

    Revert "Updated #1"

--- src/routes/peopleRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const peopleController = require('../controllers/peopleController');
-
-router.get('/', peopleController.getPeople);
-
-module.exports = router;
--- src/routes/profileRoute.js
+++
@@ -1,8 +0,0 @@
-// routes/profileRoute.js
-const express = require('express');
-const router = express.Router();
-const profileController = require('../controllers/profileController');
-
-router.get('/profile', profileController);
-
-module.exports = router;
